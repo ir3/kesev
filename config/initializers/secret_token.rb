4788d36a503e95fa6568274896547bb8d3435dbf
FUKUI Osamu <ofukui@irubysystems.com>
2012-05-10 13:01:10 +0900

    kesev start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kesev::Application.config.secret_token = 'b927bd21bfe498aff1ca9b6ccb01e39f851332184a5a9d9f2f0a3bba9b564ad53ba3dab7b12f880fface3f69a9ea43cadc089d799ead7b5346efa3792577dee0'
